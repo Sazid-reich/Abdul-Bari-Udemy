7c308013e720bc58af6f3edac4bbcf67ef50080c
kanon <kanon8308@gmail.com>
2021-11-11 00:26:49 +0600

    .

---
+++ DSA/Recursion/Taylor-Series-Recusion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+double e(double x, double n)
+{
+    static double powerVariable = 1, factVariable = 1;
+    double sumOfPrev;
+
+    if (n == 0)
+        return 1;
+    sumOfPrev = e(x, n - 1);
+    powerVariable = powerVariable * x;
+    factVariable = factVariable * n;
+    return sumOfPrev + (powerVariable / factVariable);
+}
+
+int main()
+{
+    printf("%lf\n", e(4, 10));
+    return 0;
+}
