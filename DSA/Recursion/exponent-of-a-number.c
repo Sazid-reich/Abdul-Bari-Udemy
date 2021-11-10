2fcb3fe64cdace4c7176eb9ab6ab0fb4dfb303d6
kanon <kanon8308@gmail.com>
2021-11-10 14:11:20 +0600

    .

---
+++ DSA/Recursion/exponent-of-a-number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int power(int m, int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return power(m, n - 1) * 5;
+}
+
+int main()
+{
+    printf("%d\n", power(5, 3));
+}
