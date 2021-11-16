45c206033b3270573d524186a6a9e290f0131c02
kanon <kanon8308@gmail.com>
2021-11-16 13:58:16 +0600

    nCr-using-pascal-triangle

---
+++ DSA/Recursion/nCr-using-pascal-triangle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int c(int n, int r)
+{
+    if (r == 0 || n == r)
+        return 1;
+    else
+        return c(n - 1, r - 1) + c(n - 1, r);
+}
+
+void main()
+{
+    printf("%d\n", c(6, 3));
+}
